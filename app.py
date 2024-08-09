790d2c18bccd70e10903dfb3385a1fdd7033da59
ShivangRustagi04 <shivangrustagi04@gmail.com>
2024-08-09 23:56:25 +0530

    "added the transcript video details"

--- app.py
+++ app.py
@@ -6,3 +6,22 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 
 load_dotenv()
+
+genz.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt = ''' you are a yt video summarizer. i will upload youtube video link and you will access the link and summarize the content of video for me giving important summary in brief with around 300 words. Please provide the summary of video here:   '''
+
+
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id=youtube_video_url.split("=")[1]
+
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+
+
+
